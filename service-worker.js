7dd1c5d0c5c12c5437b1a75dab3eff1e208da155
Leslie J. Matthews <leslie@applehilltech.com>
2025-04-06 13:37:18 -0400

    added favicon cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "hydrolab-cache-v1";
 const urlsToCache = [
-  "./",
+  "./favicon.ico",
   "./index.html",
   "./manifest.json",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
