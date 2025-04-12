ca02cf8811c91c4a52c3bc67cc34c29488aec838
Leslie J. Matthews <leslie@applehilltech.com>
2025-04-12 14:36:05 -0400

    switching to azure web service app

---
+++ hydrolab-api/HydrolabApi/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
